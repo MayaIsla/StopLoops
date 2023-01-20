e3e212db4ae19d9da0b5a2a0fdd5e2230818f610
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2023-01-20 13:11:33 -0600

    Add files via upload

---
+++ forLoops.ps1
@@ -0,0 +1,32 @@
+
+$Body = @{
+    tenant = "tenant"
+    username = "username"
+    password = "pw"
+    role = "Admin"
+}
+
+$authCode = Invoke-RestMethod -Method 'POST' -Uri "https://becn-stg.saasit.com/api/rest/authentication/login" -Body $body
+
+
+$headers = @{
+    Authorization=$authCode
+    Content='application/json'
+}
+
+$SubjectLine = Read-Host "Enter Incident Subject Line"
+
+
+$searchQuery = Invoke-RestMethod -Method 'GET' -Uri "https://becn-stg.saasit.com/api/odata/businessobject/incidents?`$filter=Subject eq '$SubjectLine'&?filter=Status eq 'Logged'" -Headers $headers 
+
+forEach ($object in $searchQuery.value)
+{
+    $recIdofIncident = $object.recID
+    Write-Output "Deleting Inc#" $object.IncidentNumber
+    Invoke-RestMethod -Method 'DELETE' -Uri "https://becn-stg.saasit.com/api/odata/businessobject/incidents('$recIdofIncident')" -Headers $headers
+}
+
+
+
+
+
